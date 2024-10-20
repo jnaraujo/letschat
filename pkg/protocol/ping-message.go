f7c700c5ee6c8e60e2889a27ada3ca42486f0fd9
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-10-20 15:50:34 -0300

    feat: add ping message

---
+++ pkg/protocol/ping-message.go
@@ -0,0 +1,22 @@
+package protocol
+
+import "encoding/json"
+
+type PingMessage struct {
+}
+
+func PingMessageFromPacket(pkt Packet) (PingMessage, error) {
+	var msg PingMessage
+	if err := json.Unmarshal(pkt.Payload, &msg); err != nil {
+		return msg, err
+	}
+	return msg, nil
+}
+
+func (msg PingMessage) ToPacket() Packet {
+	payload, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+	return NewPacket(PacketTypePing, payload)
+}
